cd55f991190b86f804ed76d00bbb98d151c06b19
Raunak Wete <raunakwete43@gmail.com>
2026-05-12 17:14:01 +0530

    Fixed hyprland 0.55 breaking changes

--- examples/prevent-burn-in.sh
+++ examples/prevent-burn-in.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-# Call me every now and then using cron
+notify-send "Shifting pixels..."
+sleep 1
 
-# Render overlay over bar (swap each call)
-hyproled -s -a 0:0:2880:46
+# Render overlay over bar
+hyproled -s -a 0:0:2880:48
 
-# More precoutions in addition to hyproled
+in=3
+out=3
 
-# Change gaps to prevent the borders burning in
-in=3;
-out=6;
-if [ "$(hyprshade current)" == "hyproled_shader" ]; then
-    in=2;
-    out=4;
+if [ "$(hyprshade current)" = "hyproled_shader" ]; then
+    in=1
+    out=1
 fi
 
-hyprctl --instance 0 keyword general:gaps_in $in;
-hyprctl --instance 0 keyword general:gaps_out $out;
+hyprctl eval "hl.config({ general = { gaps_in = $in, gaps_out = $out } })"
